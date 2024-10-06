0321335a3c85533fcae706342817663153e198ec
trollzero <80132943+trollzero@users.noreply.github.com>
2024-10-06 14:37:42 -0400

    added time and n_term print statements

--- pi_calculation.py
+++ pi_calculation.py
@@ -1,9 +1,18 @@
 import random
+from time import perf_counter
 
 def calculate_pi_leibniz():
     n_terms = random.randint(10_000_000, 1_000_000_000)
+    print(n_terms)
+    
     pi_approx = 0
+    
+    start_time = perf_counter()
     for i in range(n_terms):
         pi_approx += ((-1)**i) / (2*i + 1)
+        
+    end_time = perf_counter()
+    
+    print(f"Time taken: {end_time - start_time}")
     return 4 * pi_approx
 
