332930d18b2677fa01e56b75bbcc3ada1e0d62ee
trollzero <80132943+trollzero@users.noreply.github.com>
2024-10-06 14:35:37 -0400

    updated pi_calculation to generate a random int between 10 million and 1 billion

--- pi_calculation.py
+++ pi_calculation.py
@@ -1,4 +1,7 @@
-def calculate_pi_leibniz(n_terms):
+import random
+
+def calculate_pi_leibniz():
+    n_terms = random.randint(10_000_000, 1_000_000_000)
     pi_approx = 0
     for i in range(n_terms):
         pi_approx += ((-1)**i) / (2*i + 1)
