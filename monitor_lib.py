d598731b94cfbe190591a2839e1e7d3185500dc1
Fernando Martínez <f.martinezlpz@gmail.com>
2024-10-10 11:21:04 -0400

    updated bash and monitor file

--- monitor_lib.py
+++ monitor_lib.py
@@ -1,43 +1,24 @@
-import socket
-import time
-import json
-
+import os
 
 def get_cpu_status(path='/proc/loadavg'):
-    '''
-        cpu usage
-    '''
     loadavg = {}
+    cpu_count = os.cpu_count()
 
     with open(path, 'r') as f1:
-
         list_content = f1.read().split()
-
-        loadavg['lavg_1'] = list_content[0]
-
-        loadavg['lavg_2'] = list_content[1]
-
-        loadavg['lavg_15'] = list_content[2]
+        
+        loadavg['lavg_1'] = calculate_percentage(float(list_content[0]), cpu_count)
+        loadavg['lavg_5'] = calculate_percentage(float(list_content[1]), cpu_count)
+        loadavg['lavg_15'] = calculate_percentage(float(list_content[2]), cpu_count)
 
     return loadavg
 
+def calculate_percentage(load, cpu_count):
+    usage_percentage = (load / cpu_count) * 100
+    return min(usage_percentage, 100)  # cap at 100%
 
-def get_memory_status(path='/proc/meminfo'):
-    '''
-       memory usage
-    '''
-    mem_dic = {}
-
-    with open(path, 'r') as f2:
-
-        lines = f2.readlines()
-
-        for line in lines:
-
-            name = line.strip().split(':')[0]
-
-            data = line.split(":")[1].split()[0]
-
-            mem_dic[name] = float(data)
-
-    return mem_dic
+# Example usage
+cpu_status = get_cpu_status()
+print(f"1-minute load average: {cpu_status['lavg_1']:.2f}%")
+print(f"5-minute load average: {cpu_status['lavg_5']:.2f}%")
+print(f"15-minute load average: {cpu_status['lavg_15']:.2f}%")
--- start_lab1.sh
+++ start_lab1.sh
@@ -1,14 +1,31 @@
-# pull the GitHub repository
-git clone https://github.com/fernando-ml/distributed_systems_lab1.git -b main
-cd distributed_systems_lab1
-
 hostname=$(hostname)
+
+# install pip3 and matplotlib in case they are not installed
+install_dependencies() {
+    echo "Checking for pip3..."
+    if ! command -v pip3 &> /dev/null; then
+        echo "pip3 not found. Installing pip3..."
+        sudo apt-get update
+        sudo apt-get install -y python3-pip
+    else
+        echo "pip3 is already installed."
+    fi
+
+    echo "Checking for matplotlib..."
+    if ! python3 -c "import matplotlib" &> /dev/null; then
+        echo "matplotlib not found. Installing matplotlib..."
+        pip3 install -U matplotlib
+    else
+        echo "matplotlib is already installed."
+    fi
+}
 
 # verify if the hostname contains "server" or "worker"
 if [[ $hostname == *"server"* ]]; then
-    python3 server.py
+    install_dependencies
+    python3 server.py # run the server script
 elif [[ $hostname == *"worker"* ]]; then
-    python3 worker.py
+    python3 worker.py # execute the worker script
 else
     echo "Error: VM's name does not contain 'server' or 'worker'"
     exit 1
